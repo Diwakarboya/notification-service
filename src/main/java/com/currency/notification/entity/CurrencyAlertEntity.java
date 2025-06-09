91316e364c1c10f056707cd8da501254d32b1c02
Diwakarboya <47384907+Diwakarboya@users.noreply.github.com>
2025-06-09 00:11:35 -0400

    changes

---
+++ src/main/java/com/currency/notification/entity/CurrencyAlertEntity.java
@@ -0,0 +1,31 @@
+package com.currency.notification.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="currency_threshold")
+public class CurrencyAlertEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String currency1;
+
+    @Column(nullable = false)
+    private String currency2;
+
+    @Column(nullable = false)
+    private double threshold;
+
+    @Column(nullable = false)
+    private boolean notifyOnDrop;
+
+
+}
